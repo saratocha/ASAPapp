649eb5c23ea154bbc8184a8038bb03e6da12027d
Sara Tocha <saratocha@gmail.com>
2022-08-07 14:57:45 +0100

    ASAPappupdate

--- app/src/main/java/com/example/asap/log.java
+++ app/src/main/java/com/example/asap/log.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class log extends AppCompatActivity {
     public Button button;
@@ -15,14 +17,26 @@ public class log extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
 
+        TextView username =(TextView) findViewById(R.id.email);
+        TextView password =(TextView) findViewById(R.id.password);
+
         button = (Button) findViewById(R.id.loginbtn);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent= new Intent(log.this,lista.class);
-                startActivity(intent);
+                if (username.getText().toString().equals("upskill@gmail.com") && password.getText().toString().equals("12345678")) {
+                    Toast.makeText(getApplicationContext()
+                            , "Form Validate Successfully", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(log.this, lista.class);
+                    startActivity(intent);
+                    return;
+                }
+
+                Toast.makeText(getApplicationContext()
+                        , "Validation Failed", Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 }
--- app/src/main/java/com/example/asap/logparceiros.java
+++ app/src/main/java/com/example/asap/logparceiros.java
@@ -6,21 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class logparceiros extends AppCompatActivity {
 public Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logparceiros);
+
+        TextView username =(TextView) findViewById(R.id.email);
+        TextView password =(TextView) findViewById(R.id.password);
+
         button = (Button) findViewById(R.id.loginbtnparc);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent= new Intent(logparceiros.this,parceiros.class);
-                startActivity(intent);
+                if (username.getText().toString().equals("sharksurf@gmail.com") && password.getText().toString().equals("sharksurf")) {
+                    Toast.makeText(getApplicationContext()
+                            , "Form Validate Successfully", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(logparceiros.this, parceiros.class);
+                    startActivity(intent);
+                    return;
+                }
+
+                Toast.makeText(getApplicationContext()
+                        , "Validation Failed", Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 }
